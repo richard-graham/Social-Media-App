ba5c9efadda35bef3d69330f989e5ca963571b92
richard-graham <richardcolingraham@gmail.com>
2019-04-05 16:04:55 +1300

    added createSCream function to firebase

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -19,7 +19,7 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     .get()
     .then(data => {
       let screams = []
-      data.forEach(doc => {
+      data.forEach(doc => { // doc = a document reference
         screams.push(doc.data()) // data() is a function that returns the data within the document
       })
       return res.json(screams)
@@ -29,3 +29,26 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     })
 })
 
+exports.createScream = functions.https.onRequest((req, res) => {
+  // validate request
+  if(req.method !== 'POST'){
+    return res.status(400).json({ error: 'Method not allowed, use a POST request' })
+  }
+  
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  } 
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully ` })
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'something went wrong '}) // note: changes status code from 200
+      console.error(err)
+    })
+})
