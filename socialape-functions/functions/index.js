bedebcdda02989f7d0b8dd6f542f7db946a4efc0
richard-graham <richardcolingraham@gmail.com>
2019-04-05 21:08:56 +1300

    sign in validation added

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -54,6 +54,8 @@ app.post('/scream', (req, res) => {
     createdAt: new Date().toISOString() // recognised time type
   } 
 
+  let token, userId // init token and userId
+
   db
     .collection('screams')
     .add(newScream)
@@ -66,6 +68,21 @@ app.post('/scream', (req, res) => {
     })
 })
 
+const isEmpty = (string) => {
+  if (string.trim() === '') return true
+  else return false 
+}
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if(email.match(regEx)){
+    return true
+  } else {
+    return false
+  }
+}
+
+
 //Sign Up Route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -74,6 +91,22 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   }
+
+  // USER VALIDATION
+
+  let errors = {}
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty'
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address'
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+  if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors)
 
   db
     .doc(`/users/${newUser.handle}`)
@@ -91,15 +124,29 @@ app.post('/signup', (req, res) => {
     })
     .then(data => {
       // request token
+      userId = data.user.uid
       return data.user.getIdToken()
     })
-    .then(token => {
-      // get token
+    .then(idToken => {
+      token = idToken
+      const userCredentials = {
+        handle: newUser.handle,
+        email:newUser.email,
+        createdAt: new Date().toISOString(),
+        userId: userId
+      }
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+    })
+    .then(() => {
       return res.status(201).json({ token })
     })
     .catch(err => {
       console.error(err)
-      return res.status(500).json({ error: err.code })
+      if(err.code === 'auth/email-already-in-use'){
+        return res.status(400).json({ email: 'Email is already in use' })
+      } else {
+        return res.status(500).json({ error: err.code })
+      }
     })
 })
 
