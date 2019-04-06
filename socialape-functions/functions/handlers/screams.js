6d504fef692621ab51e607cd5ffb1ae9bf714f40
richard-graham <richardcolingraham@gmail.com>
2019-04-06 21:16:16 +1300

    made unlike scream func

--- socialape-functions/functions/handlers/screams.js
+++ socialape-functions/functions/handlers/screams.js
@@ -155,3 +155,46 @@ exports.likeScream = (req, res) => {
       res.status(500).json({ error: err.code })
     })
 }
+
+exports.unlikeScream = (req, res) => {
+  // check to see if scream already liked & check if scream exists
+  const likeDocument = db.collection('likes')
+                        .where('userHandle', '==', req.user.handle)
+                        .where('screamId', '==', req.params.screamId)
+                        .limit(1) // will return arr with 1x doc
+  const screamDocument = db.doc(`/screams/${req.params.screamId}`)
+
+  let screamData
+
+  screamDocument
+    .get()
+    .then(doc => {
+      if(doc.exists){
+        screamData = doc.data()
+        screamData.screamId = doc.id
+        return likeDocument.get()
+      } else {
+        return res.status(404).json({ error: 'Scream not found' })
+      }
+    })
+    .then(data => {
+      if(data.empty){
+        return res.status(400).json({ error: 'Scream not liked' })
+      } else { 
+        return db
+          .doc(`/likes/${data.docs[0].id}`)
+          .delete()
+          .then(() => {
+            screamData.likeCount--
+            return screamDocument.update({ likeCount: screamData.likeCount })
+          })
+          .then(() => {
+            res.json(screamData)
+          })
+      }
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ error: err.code })
+    })
+}
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -18,8 +18,8 @@ app.get('/screams', getAllScreams)
 app.get('/scream/:screamId', getScream)
 app.post('/scream', FBAuth, postOneScream)
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
-app.post('/scream/:screamId/like', FBAuth, likeScream)
-// app.post('/scream/:screamId/unlike', FBAuth, unlikeScream)
+app.get('/scream/:screamId/like', FBAuth, likeScream)
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream)
 // TODO
 // deletescream
 // likescream
