54e0f3d36541be0f04761f824f5b7b8b3cecc970
richard-graham <richardcolingraham@gmail.com>
2019-04-07 00:45:55 +1300

    added a few fixes for returning errors

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -64,7 +64,7 @@ exports.signup = (req, res) => {
       } else if(err.code === 'auth/weak-password'){
         return res.status(400).json({ password: 'Weak password please try again' })
       } else {
-        return res.status(500).json({ error: err.code })
+        return res.status(500).json({ general: 'Something went wrong please try again' })
       }
     })
 }
@@ -96,10 +96,7 @@ exports.login = (req, res) => {
     })
     .catch(err => {
       console.error(err)
-      if(err.code = "auth/wrong-password") {
-        return res.status(403).json({ general: 'Wrong credentials, please try again' })
-      }
-      return res.status(500).json({ error: err.code })
+      return res.status(403).json({ general: 'Wrong credentials, please try again' })
     })
 }
 
