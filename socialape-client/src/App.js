37c3e8c0c9dd76c1f9fc2da9c4cdd655f4028824
richard-graham <richardcolingraham@gmail.com>
2019-04-11 09:49:35 +1200

    added MuiTHemeProvider with color palette

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom'
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 
 // Components
 import Navbar from './components/Navbar'
@@ -9,10 +12,28 @@ import home from './pages/home'
 import login from './pages/login'
 import signup from './pages/signup'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+    contrastText: '#fff'
+    }
+  }
+})
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={theme} >
+        <div className="App">
         <Router>
         <Navbar />
           <div className='container'>
@@ -24,6 +45,7 @@ class App extends Component {
           </div>
         </Router>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
