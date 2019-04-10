132c367265eab86c069ef30910de951d1a2c09cd
richard-graham <richardcolingraham@gmail.com>
2019-04-11 11:33:17 +1200

    Added scream Cards

--- socialape-client/src/pages/home.js
+++ socialape-client/src/pages/home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Grid from '@material-ui/core/Grid'
 
+import Scream from '../components/Scream'
+
 export class home extends Component {
   state = {
     screams: null
@@ -22,7 +24,7 @@ export class home extends Component {
 
   render() {
     let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map(scream => <p>{scream.body}</p>)
+      this.state.screams.map(scream => <Scream scream={scream} />)
     ) : <p>Loading...</p>
     return (
      <Grid container spacing={16}>
