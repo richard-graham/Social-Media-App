ffed4d0396ef888c517139fc324e3b402fcfe34f
richard-graham <richardcolingraham@gmail.com>
2019-04-11 21:40:41 +1200

    basic authentication added with some primitive conditional rendering

---
+++ socialape-client/src/util/theme.js
@@ -0,0 +1,44 @@
+export default {
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+    contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true,
+  },
+  form: {
+    textAlign: 'center'
+  },
+  icon: {
+    maxWidth: '80px',
+    margin: '20px auto'
+  },
+  pageTitle: {
+    margin: '10px auto',
+  },
+  textField: {
+    margin: '10px auto'
+  },
+  button: {
+    marginTop: 20,
+    position: 'relative'
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    margin: '10px auto'
+  },
+  buttonProgress: {
+    position: 'absolute'
+  },
+}
