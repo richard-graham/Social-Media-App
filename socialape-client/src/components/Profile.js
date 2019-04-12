d91a8fd14b576b1759d5d6affac6ac41071b955f
richard-graham <richardcolingraham@gmail.com>
2019-04-12 15:14:13 +1200

    brought in material-ui/icons

---
+++ socialape-client/src/components/Profile.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+//MUI
+import Button from '@material-ui/core/Button'
+
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Profile
