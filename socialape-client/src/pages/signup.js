e86db9dd6b263ea21499567060ff110a7d3decec
richard-graham <richardcolingraham@gmail.com>
2019-04-12 14:40:46 +1200

    signup form functioning with redux. Infinite reload seems to occur if the user accesses the page already with a token

--- socialape-client/src/pages/signup.js
+++ socialape-client/src/pages/signup.js
@@ -11,6 +11,10 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
+// redux
+import { connect } from 'react-redux'
+import { signupUser } from '../redux/actions/userActions'
+
 const styles = (theme) => ({
   ...theme
 })
@@ -23,8 +27,13 @@ export class signup extends Component {
       password: '',
       confirmPassword: '',
       handle: '',
-      loading: false,
       errors: {}
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.UI.errors){
+      this.setState({ errors: nextProps.UI.errors })
     }
   }
 
@@ -39,21 +48,7 @@ export class signup extends Component {
       confirmPassword: this.state.password,
       handle: this.state.handle
     }
-    axios.post('/signup', newUserData)
-      .then(res => {
-        console.log(res.data);
-        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`) // saves token to local storage in case of page refresh etc
-        this.setState({
-          loading: false
-        })
-        this.props.history.push('/')
-      })
-      .catch(err => {
-        this.setState({
-          errors: err.response.data,
-          loading: false
-        })
-      })
+    this.props.signupUser(newUserData, this.props.history)
   }
 
   handleChange = (event) => {
@@ -63,8 +58,8 @@ export class signup extends Component {
   }
 
   render() {
-    const { classes } = this.props
-    const { errors, loading } = this.state
+    const { classes, UI: { loading } } = this.props
+    const { errors } = this.state
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
@@ -148,8 +143,16 @@ export class signup extends Component {
 }
 
 signup.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+  signupUser: PropTypes.func.isRequired
 }
 
-export default withStyles(styles)(signup)
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI
+})
+
+export default connect(mapStateToProps, { signupUser })(withStyles(styles)(signup))
  
